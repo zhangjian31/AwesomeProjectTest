e3e7d1dc0361775f99b71ecaf6bce6255c533c8f
zhangjian90 <zhangjian90@maoyan.com>
2019-05-06 22:03:42 +0800

    update

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ import Test from './src/Test';
 import StateTest from './src/StateTest';
 import CityList from './src/CityList';
 import First from './src/First';
+import {Abc} from './src/高度和宽度';
+import widthAndHeight from './src/高度和宽度';
 import {name as appName} from './app.json';
+
 
 // AppRegistry.registerComponent(appName, () => App);
 // AppRegistry.registerComponent(appName, () => Test); 
 // AppRegistry.registerComponent(appName, () => StateTest); 
 // AppRegistry.registerComponent(appName, () => CityList); 
-AppRegistry.registerComponent(appName, () => First); 
+// AppRegistry.registerComponent(appName, () => First); 
+// AppRegistry.registerComponent(appName, () => Abc); 
+AppRegistry.registerComponent(appName, () => widthAndHeight); 
