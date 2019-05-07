63ceaf087f9088782c173e162ee09ee556be5a55
zhangjian90 <zhangjian90@maoyan.com>
2019-05-07 19:58:10 +0800

    textinput

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ import 电影列表 from './src/电影列表';
 import 组件 from './src/组件';
 import 网易新闻 from './src/新闻/网易新闻';
 
-import TouchableOpacityCom from './src/TouchableOpacity/TouchableOpacityDemo';
+// import TouchableOpacityCom from './src/TouchableOpacity/TouchableOpacityDemo';
+import TextInputCom from './src/TextInput/TextInputDemo';
+
 
 
 
@@ -31,5 +33,7 @@ import {name as appName} from './app.json';
 // AppRegistry.registerComponent(appName, () => 电影列表); 
 // AppRegistry.registerComponent(appName, () => 组件); 
 // AppRegistry.registerComponent(appName, () => 网易新闻); 
-AppRegistry.registerComponent(appName, () => TouchableOpacityCom); 
+// AppRegistry.registerComponent(appName, () => TouchableOpacityCom); 
+AppRegistry.registerComponent(appName, () => TextInputCom); 
+
 
---
+++ src/模板.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+} from 'react-native';
+class TextInputCom extends Component {
+    render(){
+        return (
+            <View  >
+                <Text>dfafa</Text>
+            </View>
+        );
+    };
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default TouchableOpacityCom;
