d26f33fec2b9c6f7e481aca5a8294ba358b2d093
zhangjian90 <zhangjian90@maoyan.com>
2019-05-10 10:19:20 +0800

    update

---
+++ src/ScreenTest.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+} from 'react-native';
+class ScreenCom extends Component {
+    render(){
+        return (
+            <View  >
+                <Text>dfafa</Text>
+            </View>
+        );
+    };
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ScreenCom;
